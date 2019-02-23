4193f4dd5e31587298b33ee10f5b41e1557d6674
garethxiao <garethxiao@qq.com>
2019-02-23 14:36:28 +0800

    增加删除和导入solr接口

--- src/main/java/com/pinyougou/search/service/ItemSearchService.java
+++ src/main/java/com/pinyougou/search/service/ItemSearchService.java
@@ -1,12 +1,24 @@
 package com.pinyougou.search.service;
 
+
+import java.util.List;
 import java.util.Map;
 
+/********
+ * author:Aligan
+ * date2018/10/6 14:38
+ * description:Aligan
+ * version:1.0
+ ******/
+
+
 public interface ItemSearchService {
-    /**
-     * 搜索
-     * @param keywords
-     * @return
-     */
+
+    //搜索，传过来的可能是多个值，所以需要以键值对的形式封装到一个Map里面，
     public Map<String,Object> search(Map searchMap);
+
+    //批量化导入数据到solr；
+    void importList(List list);
+
+    void deletByGoodsId(List list);
 }
